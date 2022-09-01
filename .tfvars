e372a0ee91d1d5df3be6a481a4130288702ded9b
Egorov <egor35@yandex.ru>
2022-09-01 11:05:45 +0200

    dev

---
+++ .tfvars
@@ -0,0 +1,3 @@
+token = "y0_AgAAAABjv7fsAATuwQAAAADNd1PsLO8otNcKSu-8ypOLZddyTkPTeQs"
+cloud_id = "b1g9q49j8o9p8fdicr7g"
+folder_id = "b1gphfspgsih2mk1gt8o"
---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "token" {
+  description = "Yandex Cloud security OAuth token"
+  default     = "nope"
+}
+
+variable "folder_id" {
+  description = "Yandex Cloud Folder ID where resources will be created"
+  default     = "enter your folder id"
+}
+
+variable "cloud_id" {
+  description = "Yandex Cloud ID where resources will be created"
+  default     = "there is cloud id"
+}
